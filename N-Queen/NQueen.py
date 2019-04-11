7f98cca23b7bd69bf4e67a4b1dbae0c713fffe60
ecostrophe <ecostrophe87@gmail.com>
2019-04-11 01:20:16 +0100

    Update NQueen.py
    Co-Authored-By: eco strophe <ecostrophe@users.noreply.github.com>

--- N-Queen/NQueen.py
+++ N-Queen/NQueen.py
@@ -8,14 +8,14 @@ def slove(number,nqueen):
 	if creatgrid() == True:
 		for item in grid:
 			print(item)
-	else:
-		print("there is no grid created")
-		
-	if findallposibility(grid):
-		print(nextrow)
-	else:
-		print("there is 0 queen at present")
-		
+
+	for y in range(number):
+		for j in range(number):
+			grid[y][j]=queen
+			if findallposibility(y,j)==True:
+				print("===========================")
+			else:
+				print("there is 0 queen at present")	
 #___ fonction creat grid:
 def creatgrid():
 	global number,case
@@ -23,26 +23,15 @@ def creatgrid():
 	for i in range(number):
 		grid.append(row)
 	return True
-	
-#___ fonction find all posibility:
+#___ fonction find all possibility:
 sol=[]
-def findallposibility(grid):
-	global nextrow
-	for x in grid:
-		if x.index(queen)==0:
-			nextrow=x.copy()
-			nextrow.remove(x[1])
-			nextrow.remove(x[0])
-			return nextrow
-		elif x.index(queen)==(number-1):#_____
-			nextrow=x.copy()
-			nextrow.remove(x[-2])
-			nextrow.remove(x[-1])
-			return nextrow
-		else:
-			return nextrow
-	else:
-		return True
+def findallposibility(y,j):
+	global nextrow,sol
+	if 
+
+
+
+
 #__ fonction check posibility:
 def checkposibility():
 	return
