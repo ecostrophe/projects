0456b5c014bb0ab523aa3244cb33bf9a763139e2
ecostrophe <ecostrophe87@gmail.com>
2019-04-13 22:26:02 +0100

    Update NQueen.py

--- N-Queen/NQueen.py
+++ N-Queen/NQueen.py
@@ -1,21 +1,18 @@
 case="-"
 queen="Q"
 nqueen=0
-nextrow=[]
+sol=[]
 grid=[]
 number=int(input("Please enter the number of queen:\n"))
 def slove(number,nqueen):
 	if creatgrid() == True:
+		print("creation of grid of N-Queen Problem for N=",number) # text for test only
 		for item in grid:
 			print(item)
-
-	for y in range(number):
-		for j in range(number):
-			grid[y][j]=queen
-			if findallposibility(y,j)==True:
-				print("===========================")
-			else:
-				print("there is 0 queen at present")	
+	if findallposibility(grid):
+		print("creation of grid with possible Queen positions") #text for test only
+		for item in sol:
+			print(item)
 #___ fonction creat grid:
 def creatgrid():
 	global number,case
@@ -24,19 +21,36 @@ def creatgrid():
 		grid.append(row)
 	return True
 #___ fonction find all possibility:
-sol=[]
-def findallposibility(y,j):
-	global nextrow,sol
-	if 
+def findallposibility(grid):
+	global queen, case, nqueen, sol
+	for row in grid:
+		for z in range (number):
+			row[z]=queen
+			checkbyrow(row)
+			if checkbyrow(row):
+				nqueen+=1
+			else:
+				row[z]=case
+	return sol
+#__ fonction to check evry row in grid
+def checkbyrow(row):
+	global sol,queen,case
+	if len(sol)==0:
+		sol.append(row)
+		return True
+	else:
+		for prop in sol:
+			if row.index(queen)== prop.index(queen):
+				return False
+			else:
+				return True
 
 
-
-
+# fonction to check evry column in grid
+def checkbycol():
+	return
 #__ fonction check posibility:
 def checkposibility():
 	return
-def checkbyrow():
-	return
-def checkbycol():
-	return
+
 slove(number,nqueen)
